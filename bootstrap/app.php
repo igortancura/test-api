a03f63a71078f2a5360983b92ca6fb3813da9fc0
Igor Tancura <igortancura@gmail.com>
2025-05-20 18:01:02 +0300

    fix MethodNotAllowedHttpException for api

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Exceptions\Api\ValidationApiException;
 use App\Http\Resources\Api\Message\InvalidMessageResource;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,5 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (ValidationApiException $e) {
             return new InvalidMessageResource($e->validator->errors()->toArray(), 400);
+        });
+        $exceptions->render(function (MethodNotAllowedHttpException $e) {
+            if (request()->is('api/*')) {
+                return new InvalidMessageResource(['message' => $e->getMessage()], 400);
+            }
         });
     })->create();
